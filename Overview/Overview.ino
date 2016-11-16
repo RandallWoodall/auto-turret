dc440615c514e76455534109991194b4acf7066d
BalefulShadow <matt.bredin@gmail.com>
2016-11-15 22:48:42 -0700

    Update Overview.ino

--- Overview/Overview.ino
+++ Overview/Overview.ino
@@ -1,11 +1,27 @@
+#include <Servo.h>
+
 volatile int posX[];
 volatile int posY[];
 volatile int posQuad[];
 volatile int dist[];
 volatile int i = 0;
-volatile double angleX;
-volatile double angleY;
 
+/*Stepper Motor Setup*/
+//.9 degrees per step
+const int stepDir 8;
+const int stepStp 9;
+/*Gear Conversion*/
+double gearRat = 2 / 1;             //Gear rotations per stepper rotations
+double gridRatHorz = 30/930;        //Horizontal grid ratio
+double gridRatVert = 30/1023;       //Vertical grid ratio
+int stepQuad = (30 * gearRat) / .9; //How many steps to turn 30 degrees
+
+/*Servo Setup*/
+const int servPin 10;
+volatile int vertPos = 0;
+
+/*Laser Setup*/
+const int lasPin 11;
 /*Optical Sensor pins*/
 const int opTrig 12;
 const int opEcho 13;
@@ -34,9 +50,18 @@ void setup() {
     Write_2bytes(0x33,0x33); delay(10);
     delay(100);
     //Setup for Stepper Motor
+    pinMode(stepDir, OUTPUT);
+    pinMode(stepStp, OUTPUT);
+    digitalWrite(stepDir, 0);
+    digitalWrite(stepStp, 0);
     //Setup for servo
+    myservo.attach(servPin);
     //Setup for Optical
+    pinMode(opTrig, OUTPUT);
+    pinMode(opEcho, INPUT);
     //Setup for laser
+    pinMode(lasPin, OUTPUT);
+    
 }
 
 void loop() {
@@ -58,6 +83,7 @@ void loop() {
 
 void findTarget(){
     int rot;
+    int j;
     byte irInputx[4];
     byte irInputy[4];
     //IR Sensor: 33 degrees horizontal, 23 degrees verticle view range 1023x1023 grid
@@ -65,10 +91,7 @@ void findTarget(){
     //0.032258 degrees per horizontal grid point
     //0.022483 degrees per vertical grid point
     //Turret to rotate to 6 positions (180 degree covered)
-    /*************************/
-    /*GEAR RATIOS FOR STEPPER*/
-    /*************************/
-    for(rot = 0; rot < 6; rot++){ //Track 6 rotations
+    for(rot = 0; rot < 6; rot++){       //Track 6 rotations
         /*Ready to read IR sensor*/
         Wire.beginTransmission(slaveAddress);
         Wire.write(0x36);
@@ -76,36 +99,73 @@ void findTarget(){
  
         Wire.requestFrom(slaveAddress, 16);        // Request the 2 byte heading (MSB comes first)
         /*Clear data buffer*/
-        for (i=0;i<16;i++){
+        for (i = 0; i < 16; i++){
            irBuff[i]=0; 
         }
-        i=0;
+        j = 0;
         /*Read in IR values*/
-        while(Wire.available() && i < 16) { 
-            irInputx[i] = Wire.read();
-            i++;
+        while(Wire.available() && j < 16) { 
+            irInputx[j] = Wire.read();
+            j++;
             /*MAY BE ERROR HERE!!!*/
-            irInputy[i] = Wire.read();
-            i++;
+            irInputy[j] = Wire.read();
+            j++;
             /*END POSSIBLE ERROR*/
         }
         if(irInputx != (1023 - 93) && irInputy != 1023){ //If target is found in range
             posX[i] = irInputx;                          //Store x coordinate
             posY[i] = irInputy;                          //Store y coordinate
             posQuad[i] = rot;                            //Store quadrant (0 - 5) that the target is in
+            i++;                                         //increment number of targets
         }
+        /************************************************************/
         /*May want to call findDistance here rather than in the loop*/
-        /**************************/
-        /*Rotate Turret 30 degrees*/
-        /**************************/
+        /************************************************************/
+        /*Turn 30 degrees*/
+        digitalWrite(stepDir, 1);                        //Set motor direction
+        for(j = 0; j < stepQuad; j++){                   //Step 33 times (30 degrees)
+            digitalWrite(stepStp, 1);                    //Take one step
+            delay(1);
+        }
+    }
+    /*Loop to return turret to 0 degrees*/
+    digitalWrite(stepDir, 0);                        //Set motor direction
+    for(j = 0; j < (stepQuad * 6); j++){                         //Step 33 times (30 degrees)
+        digitalWrite(stepStp, 1);                    //Take one step
+        delay(1);
     }
     //Return to main loop
 }
 
 void findDistance(){
-    /***************************************/
-    /*ADJUST TURRET ANGLES TO FIND DISTANCE*/
-    /***************************************/
+    int j;
+    int curHorz;
+    int curVert;
+    for(j = 0; j < i; j++){
+        curHorz = stepQuad * posQuad[j];
+        curHorz += (posX[j] * gridRatHorz * gearRat) / .9; //Steps to take to get to horizontal position
+        digitalWrite(stepDir, 1);                          //Set motor direction
+        for(j = 0; j < curHorz; j++){                      //Step X times (30 degrees)
+            digitalWrite(stepStp, 1);                      //Take one step
+            delay(1);
+        }
+        curVert = posY[j] * gridRatHorz;  //Find elevation
+        myservo.write(curVert);           //Adujst elevation
+        /*Read distance*/
+        digitalWrite(opTrig, LOW);      
+        delayMicroseconds(2);          
+        digitalWrite(opTrig, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(opTrig, LOW);
+        dist[j] = pulseIn(opEcho,HIGH);
+        /*Reset to default positions*/
+        myservo.write(-curVert);
+        digitalWrite(stepDir, 0);                          //Set motor direction
+        for(j = 0; j < curHorz; j++){                      //Step X times (30 degrees)
+            digitalWrite(stepStp, 1);                      //Take one step
+            delay(1);
+        }
+    }
 }
 
 void targetConf(){
@@ -115,12 +175,30 @@ void targetConf(){
 }
 
 void fire(){
-    int tar;
-    for(tar = 0; tar <= i; tar++){
-        //rotate turret to point at target
-        //Turn laser on
-        //Rinse and repeat
+    int j;
+    int curHorz;
+    int curVert;
+    for(j = 0; j < i; j++){
+        curHorz = stepQuad * posQuad[j];
+        curHorz += (posX[j] * gridRatHorz * gearRat) / .9; //Steps to take to get to horizontal position
+        digitalWrite(stepDir, 1);                          //Set motor direction
+        for(j = 0; j < curHorz; j++){                      //Step X times (30 degrees)
+            digitalWrite(stepStp, 1);                      //Take one step
+            delay(1);
+        }
+        curVert = posY[j] * gridRatHorz;  //Find elevation
+        myservo.write(curVert);           //Adujst elevation
+        /*Toggle laser*/
+        digitalWrite(lasPin, HIGH);
+        delay(1000);
+        digitalWrite(lasPin, LOW);
+        /*Reset to default positions*/
+        myservo.write(-curVert);
+        digitalWrite(stepDir, 0);                          //Set motor direction
+        for(j = 0; j < curHorz; j++){                      //Step X times (30 degrees)
+            digitalWrite(stepStp, 1);                      //Take one step
+            delay(1);
+        }
     }
-    //Return to main loop
 }
 
