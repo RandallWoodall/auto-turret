369a1f3d4b03996ad6ccbe00550c0d42f418915b
Randall Woodall <randalljwoodall@gmail.com>
2016-11-21 22:59:27 -0700

    Adding a fix for the = to == in statements.

--- Overview/Overview.ino
+++ Overview/Overview.ino
@@ -232,13 +232,13 @@ void serialEvent() {
 
 int checkState(bool prepFire) {
   //Wait here if we are in softKill.
-  while(state=softKill){}
+  while(state==softKill){}
   
   //If we are in a firing function, prepFire should be fed true, if so, wait here unless state = engage
   while((prepFire==true) && (state!=engage)){}
   
   //If at any point the state becomes hardkill, return -1
-  if(state=hardKill)
+  if(state==hardKill)
     return(-1);
   //else return 0.
   else
